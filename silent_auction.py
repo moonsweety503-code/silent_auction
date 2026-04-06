99d31c6da23ea5d9daea8d182163a7b0ba2dc764
moonsweety503-code <moonsweety503@gmail.com>
2026-04-06 10:06:22 +0500

    Add silent auction bidding system
    Implement silent auction functionality with bidding logic.

---
+++ silent_auction.py
@@ -0,0 +1,25 @@
+import os
+
+bids_finished = False
+bids = {}
+
+def highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with ${highest_bid}")
+
+while not bids_finished:
+    name = input("What is your name: ")
+    price = int(input("What is your bid: $"))
+    bids[name] = price
+    should_continue = input("Are there any other bidders? Type 'yes' or 'no': ").lower()
+    if should_continue == "no":
+        bids_finished = True
+        highest_bidder(bids)
+    elif should_continue == "yes":
+        os.system("clear")
